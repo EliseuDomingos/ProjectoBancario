b4c3e48cd293cdcf63a3d1242317093f1cafb897
Eliseu Domingos <elitdomingos@gmail.com>
2021-02-22 17:16:37 +0100

    Criação da classe pessoa

---
+++ src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/Pessoa.kt
@@ -0,0 +1,14 @@
+package ProjectoDigitalIvaonationOneProjectoBanca
+
+class Pessoa {
+
+    val nome: String = "Eliseu Domingos"
+    val cpf:String = "12.45.78.26"
+
+}
+fun main(){
+
+    val pessoa1 = Pessoa()
+    println(pessoa1.nome)
+    println(pessoa1.cpf)
+}
