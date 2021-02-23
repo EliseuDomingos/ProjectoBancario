65e62356401832e0445f5096da0ece2a1a2fa468
Eliseu Domingos <elitdomingos@gmail.com>
2021-02-23 19:56:30 +0100

    Criação da Class Banco

--- src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/Banc.kt
+++ src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/Banc.kt
@@ -1,8 +1,12 @@
 package ProjectoDigitalIvaonationOneProjectoBanca
 
+// Criação de uma dataClasse
 data class Banc(
+
+    // propriedades da dataClasse
     val nome:String,
     val numero:Int
 ){
-
+    // criação de metodo e atribuir as propriedades da classe na função
+    fun info() = "$numero + $nome"
 }
