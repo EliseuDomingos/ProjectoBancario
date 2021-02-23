7b3247c10ae64e2d9031b60cc20bcf240fd7e5f8
Eliseu Domingos <elitdomingos@gmail.com>
2021-02-23 19:57:02 +0100

    File kt - Testar o Banco

--- src/main/kotlin/Testes/BancTeste.kt
+++ src/main/kotlin/Testes/BancTeste.kt
@@ -4,6 +4,8 @@ import ProjectoDigitalIvaonationOneProjectoBanca.Banc
 
 fun main(){
 
+    // testar as clsse Banc
+    // chamar a função
     val bancoDigiOne = Banc("DigiOne", 1)
     println(bancoDigiOne.numero)
     println(bancoDigiOne.nome)
@@ -11,5 +13,7 @@ fun main(){
     val bancoDigiOne2 = bancoDigiOne.copy("DigiOne2", 2)
     println(bancoDigiOne2.numero)
     println(bancoDigiOne2.nome)
+
+    println(bancoDigiOne2.info())
 
 }
