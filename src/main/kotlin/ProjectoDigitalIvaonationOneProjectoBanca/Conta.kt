708adfe2a5b72c4dec5bc530df968c521fb30a40
Eliseu Domingos <elitdomingos@gmail.com>
2021-02-23 10:40:41 +0100

    Criação da Class conta

---
+++ src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/Conta.kt
@@ -0,0 +1,21 @@
+import java.math.BigDecimal
+
+// criação da Class
+class Conta(
+
+    // Inicialização da class // a colocar atributos na class
+    val agencia:String,
+    val numero:String,
+    val saldo:BigDecimal
+
+){
+    // criação de uma função para depositar valores
+    fun depostio(valor:BigDecimal){
+
+    }
+    // criação de uma função para fazer o saque
+    fun saque(valor: BigDecimal){
+
+    }
+
+}
