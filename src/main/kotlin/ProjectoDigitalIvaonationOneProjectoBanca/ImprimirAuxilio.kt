eb8732f3fd6beb0cac57024dbaae174c809d2259
Eliseu Domingos <elitdomingos@gmail.com>
2021-02-24 08:22:03 +0100

    Super Classe Pessoa

--- src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/ImprimirAuxilio.kt
+++ src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/ImprimirAuxilio.kt
@@ -2,6 +2,7 @@ package ProjectoDigitalIvaonationOneProjectoBanca
 
 class ImprimirAuxilio {
 
+    // classe com companion Object não tem metodo contrutor... ou seja não recebem propriedades... elas são instanciadas apartir da propria classe
     companion object {
         fun imprimirBonus(funcionario: Funcionario) = println(funcionario.toString())
     }
--- src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/Pessoa.kt
+++ src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/Pessoa.kt
@@ -1,24 +1,9 @@
 package ProjectoDigitalIvaonationOneProjectoBanca
 
-class Pessoa {
-
-    var nome: String = "Eliseu Domingos"
-    var cpf:String = "12.45.78.26"
-        private set // está linha impede com que fora dos escopo, as variaveis nome e cpf, ficassem inalterados...
-
-    constructor() // criação de um metodo construtor
-    
-    // criação de função com atribuição dos atributos da classe
-    fun pessoaInfo() = "$nome + $cpf"
-
-
-}
-fun main(){
-
-    val pessoa1 = Pessoa()
-    println(pessoa1.nome)
-    println(pessoa1.cpf)
-
-    println(pessoa1.pessoaInfo())
+abstract class Pessoa (
+     val nome: String,
+     val cpf:String
+)
+{
 
 }
