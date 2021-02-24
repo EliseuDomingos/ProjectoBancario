6e57a9db5efa7cd39e50e2a68ed4d2e38a018483
Eliseu Domingos <elitdomingos@gmail.com>
2021-02-24 08:00:14 +0100

    Class para imprimir o Bonus Salarial

---
+++ src/main/kotlin/ProjectoDigitalIvaonationOneProjectoBanca/ImprimirAuxilio.kt
@@ -0,0 +1,8 @@
+package ProjectoDigitalIvaonationOneProjectoBanca
+
+class ImprimirAuxilio {
+
+    companion object {
+        fun imprimirBonus(funcionario: Funcionario) = println(funcionario.toString())
+    }
+}
